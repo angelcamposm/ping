64ca0b23f5d47ae85d73f30f2bc27cf643e43160
Angel Campos <angel.campos.m@outlook.com>
2020-04-12 21:31:58 +0000

    Apply fixes from StyleCI

--- src/Ping.php
+++ src/Ping.php
@@ -6,21 +6,17 @@
  * This class makes Ping request to a host using Ping command from IPutils linux package
  *
  * Ping uses the ICMP protocol's mandatory ECHO_REQUEST datagram to elicit an ICMP ECHO_RESPONSE from a host or gateway.
- * 
+ *
  * @version  1.0.0
  * @author  Angel Campos <angel.campos.m@outlook.com>
  */
 
 namespace Acamposm\Ping;
 
-use Acamposm\Ping\PingCommand;
-use Acamposm\Ping\PingParser;
-use Acamposm\Ping\Timer;
-
 class Ping
 {
     /**
-     * The IP address of the host
+     * The IP address of the host.
      *
      * @var  string
      */
@@ -29,93 +25,93 @@ class Ping
     /**
      * Stop after sending count ECHO_REQUEST packets.
      *
-     * @var  integer
+     * @var  int
      */
     private $count = 4;
 
     /**
-     * Wait interval seconds between sending each packet. The default is to 
-     * wait for one second between each packet normally, or not to wait in 
-     * flood mode. 
+     * Wait interval seconds between sending each packet. The default is to
+     * wait for one second between each packet normally, or not to wait in
+     * flood mode.
      * Only super-user may set interval to values less than 0.2 seconds.
      *
-     * @var  integer
+     * @var  int
      */
     private $interval = 1;
 
     /**
-     * Specifies the number of data bytes to be sent. 
-     * The default is 56, which translates into 64 ICMP data bytes when 
+     * Specifies the number of data bytes to be sent.
+     * The default is 56, which translates into 64 ICMP data bytes when
      * combined with the 8 bytes of ICMP header data.
      *
-     * @var  integer
+     * @var  int
      */
     private $packet_size = 64;
 
     /**
-     * Time to wait for a response, in seconds. The option affects only 
+     * Time to wait for a response, in seconds. The option affects only
      * timeout in absence of any responses, otherwise ping waits for two RTTs.
      *
-     * @var  integer
+     * @var  int
      */
     private $timeout = 5;
 
     /**
-     * The TTL value of an IP packet represents the maximum number of IP 
-     * routers that the packet can go through before being thrown away.  
-     * In current practice you can expect each router in the Internet to 
+     * The TTL value of an IP packet represents the maximum number of IP
+     * routers that the packet can go through before being thrown away.
+     * In current practice you can expect each router in the Internet to
      * decrement the TTL field by exactly one.
      *
-     * @var  integer
+     * @var  int
      */
     private $time_to_live = 128;
 
     /**
-     * The Exit from the exec command
+     * The Exit from the exec command.
      *
      * @var  string[]
      */
     private $exec_result;
 
     /**
-     * An object to allow us to control the total execution time
+     * An object to allow us to control the total execution time.
      *
      * @var  stdClass
      */
     private $timer;
 
-    function __construct($host)
+    public function __construct($host)
     {
-    	$this->host = $host;
-    	$this->timer = new Timer();
+        $this->host = $host;
+        $this->timer = new Timer();
     }
 
     public static function Create($host)
     {
-    	return new static($host);
+        return new static($host);
     }
 
     /**
-     * Return an object with the options configured for the Ping (for debug purposes)
+     * Return an object with the options configured for the Ping (for debug purposes).
      *
      * @return  stdClass
      */
     public function GetPingOptions()
     {
         $options = new stdClass();
-        $options->count        = $this->count;
-        $options->interval     = $this->interval;
-        $options->packet_size  = $this->packet_size;
-        $options->timeout      = $this->timeout;
+        $options->count = $this->count;
+        $options->interval = $this->interval;
+        $options->packet_size = $this->packet_size;
+        $options->timeout = $this->timeout;
         $options->time_to_live = $this->time_to_live;
 
         return $options;
     }
 
     /**
-     * Set the total of packets to sent
+     * Set the total of packets to sent.
      *
-     * @param  integer  $count
+     * @param  int  $count
      * @return  $this
      */
     public function Count(int $count)
@@ -128,7 +124,7 @@ class Ping
     /**
      * Set interval in seconds between each packet.
      *
-     * @param  integer  $interval
+     * @param  int  $interval
      * @return  $this
      */
     public function Interval(float $interval)
@@ -139,9 +135,9 @@ class Ping
     }
 
     /**
-     * Set the packet size
+     * Set the packet size.
      *
-     * @param  integer  $size
+     * @param  int  $size
      * @return  $this
      */
     public function PacketSize(int $size)
@@ -152,9 +148,9 @@ class Ping
     }
 
     /**
-     * Set the time to wait for a response
+     * Set the time to wait for a response.
      *
-     * @param  integer  $seconds 
+     * @param  int  $seconds
      * @return  $this
      */
     public function Timeout(int $seconds)
@@ -165,9 +161,9 @@ class Ping
     }
 
     /**
-     * Set the TTL value of the IP packet
+     * Set the TTL value of the IP packet.
      *
-     * @param  integer  $ttl
+     * @param  int  $ttl
      * @return $this
      */
     public function TimeToLive(int $ttl)
@@ -178,19 +174,19 @@ class Ping
     }
 
     /**
-     * Ping a host
+     * Ping a host.
      *
      * @return  void
      */
     public function Run()
     {
-    	$command = PingCommand::Create($this->host)
-    							->Count($this->count)
-    							->Interval($this->interval)
-    							->PacketSize($this->packet_size)
-    							->Timeout($this->timeout)
-    							->TimeToLive($this->time_to_live)
-    							->Command();
+        $command = PingCommand::Create($this->host)
+                                ->Count($this->count)
+                                ->Interval($this->interval)
+                                ->PacketSize($this->packet_size)
+                                ->Timeout($this->timeout)
+                                ->TimeToLive($this->time_to_live)
+                                ->Command();
 
         exec($command, $exec_result);
 
@@ -200,7 +196,7 @@ class Ping
         $results = PingParser::Create($exec_result)->Parse();
 
         // Append to the parsed data
-        $results->options    = $this->GetPingOptions();
+        $results->options = $this->GetPingOptions();
         $results->time_taken = $this->timer->Results();
 
         //if ($parser->IsUnreachable())
