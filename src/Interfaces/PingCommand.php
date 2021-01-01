765c364a943e7e36423c79e69c905489ecacb9f7
angelcamposm <angel.campos.m@outlook.com>
2021-01-01 12:25:08 +0100

    Added method definitions

--- src/Interfaces/PingCommand.php
+++ src/Interfaces/PingCommand.php
@@ -2,19 +2,67 @@
 
 namespace Acamposm\Ping\Interfaces;
 
+use Acamposm\Ping\Exceptions\MaxValueException;
+use Acamposm\Ping\Exceptions\NegativeValueException;
+use Acamposm\Ping\Exceptions\UnknownOSException;
 use Acamposm\Ping\PingCommandBuilder;
 
 interface PingCommand
 {
+    /**
+     * Stop after sending count ECHO_REQUEST packets. With deadline option, ping
+     * waits for count ECHO_REPLY packets, until the timeout expires.
+     *
+     * @param int $count
+     * @return PingCommandBuilder
+     * @throws NegativeValueException
+     */
     public function count(int $count): PingCommandBuilder;
 
+    /**
+     * Wait interval seconds between sending each packet. The default is to wait
+     * for one second between each packet normally, or not to wait in flood mode.
+     * Only super-user may set interval to values less than 0.2 seconds.
+     *
+     * @param float $interval
+     * @return PingCommandBuilder
+     */
     public function interval(float $interval): PingCommandBuilder;
 
+    /**
+     * Specifies the number of data bytes to be sent. The default is 56, which
+     * translates into 64 ICMP data bytes when combined with the 8 bytes of ICMP
+     * header data.
+     *
+     * @param int $packet_size
+     * @return PingCommandBuilder
+     */
     public function packetSize(int $packet_size): PingCommandBuilder;
 
+    /**
+     * ping only. Set the IP Time to Live.
+     *
+     * @param int $ttl
+     * @return PingCommandBuilder
+     * @throws MaxValueException
+     */
     public function ttl(int $ttl): PingCommandBuilder;
 
+    /**
+     * Time to wait for a response. The option affects only timeout in absence
+     * of any responses, otherwise ping waits for two RTTs.
+     * (Seconds for Linux OS, Milliseconds for Windows).
+     *
+     * @param int $timeout
+     * @return PingCommandBuilder
+     */
     public function timeout(int $timeout): PingCommandBuilder;
 
+    /**
+     * Return the Ping Command.
+     *
+     * @return string
+     * @throws UnknownOSException
+     */
     public function get(): string;
 }
