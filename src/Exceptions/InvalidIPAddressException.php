7d15f53aebeb4fb5a0b6940ebc47c558cc9ce7ac
Angel <angel.campos.m@outlook.com>
2022-01-29 16:25:12 +0100

    FIX: added missing exception class...

---
+++ src/Exceptions/InvalidIPAddressException.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Ping for Laravel.
+ *
+ * This class makes Ping request to a host.
+ *
+ * Ping uses the ICMP protocol's mandatory ECHO_REQUEST datagram to elicit an ICMP ECHO_RESPONSE from a host or gateway.
+ *
+ * @author  Angel Campos <angel.campos.m@outlook.com>
+ * @requires PHP 8.0
+ *
+ * @version  2.1.2
+ */
+
+namespace Acamposm\Ping\Exceptions;
+
+use Exception;
+
+class InvalidIPAddressException extends Exception
+{
+    public function __construct()
+    {
+        parent::__construct('Invalid IP Address');
+    }
+}
